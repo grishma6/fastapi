bef24ab9eb05c18399fabb7d9d6639a7324edf60
grishma6 <gxg59150@ucmo.edu>
2026-07-11 18:34:24 -0500

    CRUD

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 # defining an object
 app = FastAPI()
@@ -9,6 +10,53 @@ def greet():
     return "Welcome to the AI world"
 
 
+products = [
+    Product(id=1, name="Phone", description="A smartphone",
+            price=699.99, quantity=50),
+    Product(id=2, name="Laptop", description="A powerful Laptop",
+            price=999.99, quantity=30),
+    Product(id=5, name="Pen", description="A blue ink pen",
+            price=1.99, quantity=100),
+    Product(id=6, name="Table", description="A wooden table",
+            price=199.99, quantity=20),
+]
+
+
 @app.get("/products")
 def get_all_products():
-    return "All Products"
+    return products
+
+
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+
+    return "product not found"
+
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
+
+
+@app.put("/product")
+def update_product(id: int, product: Product):
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
+            return "Product Added Succesfully"
+
+    return "NO product found"
+
+
+@app.delete("/product")
+def delete_product(id: int):
+    for i in range(len(products)):
+        if products[i].id == product.id:
+            products.pop(i)
+            return "Product Deleted Successfully"
+
+    return "NO product deleted"
